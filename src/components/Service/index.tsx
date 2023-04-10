1b55aea66a1a1ab9270f1ebc43edbb3234bb96d4
Guy de Messimy <gdemessimy@gmail.com>
2023-04-10 22:43:30 +0200

    services actions

---
+++ src/components/Service/index.tsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+
+function ServiceActions() {
+  return (
+    <div>
+      <Link to="/blog/new">redirection</Link>
+    </div>
+  );
+}
+
+export default ServiceActions;
