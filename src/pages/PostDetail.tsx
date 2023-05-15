d78b9086112c210fa3f87913f2baa7b872001337
Guy de Messimy <gdemessimy@gmail.com>
2023-05-15 22:17:12 +0200

    wip : post details

---
+++ src/pages/PostDetail.tsx
@@ -0,0 +1,18 @@
+// import { useLoaderData } from 'react-router-dom';
+
+function PostDetailPage() {
+  return (
+    <>
+      <p>post details</p>
+    </>
+  );
+}
+
+export default PostDetailPage;
+
+// page loaded after data fetching
+// export function loader({ params }) {
+//   const postId = params.id;
+
+//   return getPost(postId);
+// }
