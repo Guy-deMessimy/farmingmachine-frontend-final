8f13ef7108854a04e29b04debf9968bb7b6171cb
Guy de Messimy <gdemessimy@gmail.com>
2023-05-15 22:14:33 +0200

    config paths

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,8 @@ import RootLayout from './pages/RootLayout';
 import ErrorPage from './pages/Error';
 import WelcomePage from './pages/Welcome';
 import ServiceLayout from './pages/ServiceLayout';
+import BlogLayout from './pages/BlogLayout';
+import BlogPostsPage from './pages/BlogPosts';
 
 const router = createBrowserRouter([
   {
@@ -15,6 +17,21 @@ const router = createBrowserRouter([
       {
         path: '/service',
         element: <ServiceLayout />,
+      },
+      {
+        path: '/blog',
+        element: <BlogLayout />,
+        children: [
+          {
+            index: true,
+            element: <BlogPostsPage />,
+          },
+          // {
+          //   path: ':id',
+          //   element: <PostDetailPage />,
+          //   // loader: blogPostLoader,
+          // },
+        ],
       },
       {
         path: '/rent',
