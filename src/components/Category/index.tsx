b7e2736a255a3ee79fc2cd4cc5bf7fe0a759771b
Guy de Messimy <gdemessimy@gmail.com>
2023-05-28 17:01:53 +0200

    wip : categories sliders

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -1,19 +1,43 @@
-import React, { FC } from 'react';
+import React from 'react';
 import { Link } from 'react-router-dom';
 import { CategoriesProps, Category } from '../../models/CategoriesConfig';
+import { Swiper, SwiperSlide } from 'swiper/react';
+
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/pagination';
+
+// import required modules
+import { Pagination } from 'swiper';
 
 const Categories = (props: CategoriesProps) => {
   const { categoriesService } = props;
+  console.log('categoriesService', categoriesService);
+  const slides = () => {
+    <ul></ul>;
+  };
+
+  console.log('slides', slides);
   return (
-    <ul>
-      {categoriesService.map((categoriesService: Category) => (
-        <li key={categoriesService.id}>
-          <Link to={categoriesService.id.toString()}>
-            <h3>{categoriesService.title}</h3>
-          </Link>
-        </li>
-      ))}
-    </ul>
+    <>
+      <Swiper
+        slidesPerView={7}
+        spaceBetween={10}
+        pagination={{
+          clickable: true,
+        }}
+        modules={[Pagination]}
+        className="mySwiper"
+      >
+        {categoriesService.map((categoriesService: Category) => (
+          <SwiperSlide key={categoriesService.id}>
+            <Link to={categoriesService.id.toString()}>
+              <h3>{categoriesService.title}</h3>
+            </Link>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </>
   );
 };
 
