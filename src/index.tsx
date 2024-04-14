effe2397cb85efb856bbd76d08880529fa353cb4
Guy de Messimy <gdemessimy@gmail.com>
2024-04-14 13:49:41 +0200

    disable base ui styletron

--- src/index.tsx
+++ src/index.tsx
@@ -3,14 +3,9 @@ import ReactDOM from 'react-dom/client';
 import { ApolloClient, InMemoryCache, ApolloProvider, from } from '@apollo/client';
 import { createUploadLink } from 'apollo-upload-client';
 import { onError } from '@apollo/client/link/error';
-import { Provider as StyletronProvider, DebugEngine } from 'styletron-react';
-import { Client as Styletron } from 'styletron-engine-atomic';
-import { BaseProvider, LightTheme } from 'baseui';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const debug = process.env.NODE_ENV === 'production' ? void 0 : new DebugEngine();
-const engine = new Styletron();
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 
 const uploadLink = createUploadLink({
@@ -37,11 +32,7 @@ const client = new ApolloClient({
 root.render(
   <React.StrictMode>
     <ApolloProvider client={client}>
-      <StyletronProvider value={engine} debug={debug} debugAfterHydration>
-        <BaseProvider theme={LightTheme}>
-          <App />
-        </BaseProvider>
-      </StyletronProvider>
+      <App />
     </ApolloProvider>
   </React.StrictMode>,
 );
